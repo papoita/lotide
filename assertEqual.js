cafdba5ea1ccda79ce7b01ade222a65707b11d84
PaolaP <paolaperez8@gmail.com>
2021-10-22 02:54:04 +0000

    assertEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -4,11 +4,9 @@ const assertEqual = function (actual, expected) {
     console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
   }
   else if (actual !== expected) {
-    console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`)
-
+    console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`);
   }
 };
-
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
