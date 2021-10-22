b06992f18032339a71d85950e571d0f2b581294e
PaolaP <paolaperez8@gmail.com>
2021-10-22 02:47:17 +0000

    emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: [${actual}] === [${expected}]`);
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
   }
   else if (actual !== expected) {
-    console.log(`Assertion failed: [${actual}] !== [${expected}]`)
+    console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`)
 
   }
 };
