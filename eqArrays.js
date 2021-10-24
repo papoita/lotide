f5ecf0c19cb49635986256a846a7ae095ef5c55c
PaolaP <paolaperez8@gmail.com>
2021-10-24 14:32:52 -0400

    eqArrays function using assetEqual

--- eqArrays.js
+++ eqArrays.js
@@ -6,8 +6,35 @@ const assertEqual = function (actual, expected) {
     console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`);
   }
 };
-// TEST CODE
+/*// TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(2, 5);
 assertEqual(true, false);
+*/
+
+//requires a nested loop
+function eqArrays(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false
+    }
+  }
+  return true;
+}
+
+
+
+/*
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+*/
+
+//test
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
+// => should PASS
