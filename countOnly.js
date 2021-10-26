59d69be48edd41ed74c855a2ea537db388fe15c6
PaolaP <paolaperez8@gmail.com>
2021-10-25 23:03:06 -0400

    contOnly function with failed assertion tests

---
+++ countOnly.js
@@ -0,0 +1,56 @@
+
+
+//testing functions
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  }
+  else if (actual !== expected) {
+    console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//countOnly function
+//input collection of items
+//define which items to count and ignore the others
+//return counts for a specific subset of those items object
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+    return results;
+  }
+}
+
+
+
+//Test cases
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
