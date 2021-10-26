f76976375cf2ded9f8c4652c47f02dc177275225
PaolaP <paolaperez8@gmail.com>
2021-10-26 15:01:56 -0400

    function countLetters works

--- countLetters.js
+++ countLetters.js
@@ -14,21 +14,38 @@ const assertEqual = function (actual, expected) {
 //input a string
 //return a count of each of the letters
 //countLetters('LHL') should return results indicating that L appears twice, and H once
-
-const countLetters = function (allItems, itemsToCount) {
+/*
+const countLetters = function (words) {
   const results = {};
-  for (const item of allItems) {//each name in the allItems array
-    if (itemsToCount[item]) {
-      console.log([item]);
-      if (results[item]) {
-        console.log(results[item]);
-        results[item] += 1;
+  for (const letter of words) {//each letter in the allItems array
+    if(letter === ' '){
+        continue
+    }
+    if (results[letter] === undefined) {
+      results[letter] = 1;
+    }else {
+        results[letter] += 1;
+    }
+  }
+  console.log(results);
+  return results;
+}
+countLetters('I am Paola Perez');
+*/
+
+const countLetters = function (words) {
+  const results = {};
+  for (const letter of words) {//each letter in the allItems array
+    if (letter !== " ") {
+      if (results[letter] === undefined) {
+        results[letter] = 1;
       } else {
-        console.log(results[item]);
-        results[item] = 1;
+        results[letter] += 1;
       }
     }
 
-  } console.log(results);
+  }
+  console.log(results);
   return results;
 }
+countLetters('I am Paola Perez');
