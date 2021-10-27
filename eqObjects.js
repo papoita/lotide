e771e0c2247c0824d99c9dcc8e7ec66285d440a9
PaolaP <paolaperez8@gmail.com>
2021-10-27 00:32:42 -0400

    step 1

---
+++ eqObjects.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  }
+  else if (actual !== expected) {
+    console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+//actual function
+//takes in 2 objects
+//returns true false on perfect match ===
+const eqObjects = function () {
+
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc)); // => false
