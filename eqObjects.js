567076ddf20a17a1035d8cde1db8e2786d18f871
PaolaP <paolaperez8@gmail.com>
2021-10-27 23:41:12 -0400

    step 3 arrays as values test

--- eqObjects.js
+++ eqObjects.js
@@ -10,13 +10,30 @@ const assertEqual = function (actual, expected) {
 //actual function
 //takes in 2 objects
 //returns true false on perfect match ===
-const eqObjects = function () {
 
+//Object.keys(ab); //returns [a,b]
+//Object.values(object1); //returns ["2", "1"]
+
+
+
+const eqObjects = function (object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  //console.log(object1["keys"]);
+  for (const item of object1) {
+    //console.log(item);
+    //console.log(object1[item]);
+    if (object1[item] !== object2[item]) {
+      return false;
+    }
+  } return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba)); // => true
+//testing
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc)); // => false
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
