4b4ab3cb5ded1f5ddaf14a27087423f32c35bacd
PaolaP <paolaperez8@gmail.com>
2021-10-28 14:26:37 -0400

    fixed to include testing length of the array before comparisson

--- eqArrays.js
+++ eqArrays.js
@@ -15,6 +15,9 @@ assertEqual(true, false);
 
 //requires a nested loop
 function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       return false
@@ -22,7 +25,6 @@ function eqArrays(array1, array2) {
   }
   return true;
 }
-
 
 
 /*
