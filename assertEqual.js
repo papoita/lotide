c968013cf0ff0687f1fe17ab74062c6c58d25203
PaolaP <paolaperez8@gmail.com>
2021-11-09 14:20:48 -0500

    asssertEqual function no test case

--- assertEqual.js
+++ assertEqual.js
@@ -10,8 +10,3 @@ const assertEqual = function (actual, expected) {
 
 module.exports = assertEqual;
 //we are passing the variable assertEqual and not calling the function assertEqual()
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 5);
-assertEqual(true, false);
