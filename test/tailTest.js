1656e68b3a21ae4d02706983fb7c6579dee4a0a8
PaolaP <paolaperez8@gmail.com>
2021-11-10 14:06:19 -0500

    chai deepEqual test linted

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,24 +1,27 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-//console.log(result.length);
-
-
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-//assertEqual(result.length, 2);
-
-//test case 2 an array with one element
-const result1 = tail(["hello"]);
-//console.log(result1.length);
-assertEqual(result1.length, 0);
-
-//test case 3 an empty array
-const result2 = tail([]);
-//console.log(result2.length);
-assertEqual(result2.length, 0);
+describe("#tail", () => {
+  it("array [1, 2, 3] returns [2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("array ['Hello', 'Lighthouse', 'Labs'] returns ['Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("array with one element returns empty array", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  it("array with no argument returns empty array", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
 
 //linted
+
+
+
+
+
+
+
+
