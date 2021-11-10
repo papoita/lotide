732d53b041c23bb7b5c193bced7f3135efd48460
PaolaP <paolaperez8@gmail.com>
2021-11-10 15:24:47 -0500

    exported and linted

--- flatten.js
+++ flatten.js
@@ -1,8 +1,9 @@
+
 //input array with nested arrays
 //return flattened version
 //is several nests it is called recursion
 
-function flatten(elements) {
+const flatten = function(elements) {
   let flatArray = [];
   for (let i = 0; i < elements.length; i++) {
     if (Array.isArray(elements[i])) {
@@ -13,30 +14,12 @@ function flatten(elements) {
   }
   console.log(flatArray);
   return flatArray;
-}
+};
 //test
-flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
 //typeof returns objects
 //Array.isArray
 //or let flatArray = [].concat.apply([], arr);
-//recursive solution
-function assertArraysEqual(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      console.log(`[${array1}] is different to [${array2}]`);
-      return false;
-    }
-  }
-  console.log(`[${array1}] is the same to [${array2}]`);
-  return true;
-}
 
-
-function eqArrays(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    }
-  }
-  return true;
-}
+module.exports = flatten;
+//linted
