990e797e688d5d31983e9c612d846be2f274235c
PaolaP <paolaperez8@gmail.com>
2021-11-09 19:49:28 -0500

    linted

--- head.js
+++ head.js
@@ -1,14 +1,15 @@
-const assertEqual = require('./assertEqual');
 
-function head(array) {
+const head = function(array) {
   if (array.length >= 1) {
+    //console.log(array[0]);
     return array[0];
-    console.log(array[0]);
 
-  } else if (array.length = 0) {
-    console.log("undefined");
+
+  } else {
+    //console.log("undefined");
+    return undefined;
   }
-}
+};
 
 module.exports = head;
 /*
@@ -16,3 +17,4 @@ scenario handling
 An array with only one element should still yield that one element as its head
 An empty array should yield undefined as its head
 */
+//linted
