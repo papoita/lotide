a9dccf6c50c4eecec8541b971601fb3d0abe4a1c
PaolaP <paolaperez8@gmail.com>
2021-11-10 15:06:18 -0500

    exported and linted

--- countOnly.js
+++ countOnly.js
@@ -2,11 +2,10 @@
 
 //testing functions
 
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  }
-  else if (actual !== expected) {
+  } else if (actual !== expected) {
     console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`);
   }
 };
@@ -19,7 +18,7 @@ const assertEqual = function (actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {//each name in the allItems array
     if (itemsToCount[item]) {
@@ -35,7 +34,7 @@ const countOnly = function (allItems, itemsToCount) {
 
   } console.log(results);
   return results;
-}
+};
 
 
 
@@ -61,3 +60,6 @@ assertEqual(result1.Jason, 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
 assertEqual(result1["Agouhanna"], undefined);
+
+//eslint
+module.exports = countOnly;
