f7f8ed27eee13b6e103a8eb961857c49399fd156
PaolaP <paolaperez8@gmail.com>
2021-11-09 23:31:58 -0500

    refactored and linted

--- test/middleTest.js
+++ test/middleTest.js
@@ -13,3 +13,4 @@ console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
 assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3, 4]), [2]);
 assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+//linted
