1e16c499d172c9d45c48044b4526990f5af0192d
PaolaP <paolaperez8@gmail.com>
2021-11-09 19:44:36 -0500

    linted

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,7 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  }
-  else if (actual !== expected) {
-    console.log(`❌ Assertion failed: [${actual}] !== [${expected}]`);
-  }
-};
-/*// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 5);
-assertEqual(true, false);
-*/
+
 
 //requires a nested loop
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -24,19 +11,6 @@ const eqArrays = function (array1, array2) {
     }
   }
   return true;
-}
-
-
-/*
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-*/
-
-//test
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
-// => should PASS
+};
+module.exports = eqArrays;
+//linted
