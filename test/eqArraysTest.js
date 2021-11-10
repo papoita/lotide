996f31b723ecb4313cb539615b5bec834d410053
PaolaP <paolaperez8@gmail.com>
2021-11-09 19:52:18 -0500

    linted

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -14,3 +14,4 @@ eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
 // => should PASS
+//linted
