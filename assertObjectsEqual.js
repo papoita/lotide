c8a1ddb851e56c8d37a5cfec665b311415b68bd9
PaolaP <paolaperez8@gmail.com>
2021-11-09 23:37:17 -0500

    linted

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,16 +1,6 @@
-function eqArrays(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
@@ -31,7 +21,7 @@ const eqObjects = function (object1, object2) {
 
 
 
-const assertObjectsEqual = function (object1, object2) {
+const assertObjectsEqual = function(object1, object2) {
   const inspect = require("util").inspect;
 
   if (eqObjects(object1, object2)) {
