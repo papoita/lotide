ae529d7a3234dd1510cb16a88c2bca534a779982
PaolaP <paolaperez8@gmail.com>
2021-11-10 19:15:02 -0500

    added test cases

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -13,5 +13,8 @@ eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 //test
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false);
+assertEqual(eqArrays([1, 2, "3"], [1, 2, "3"]), true);
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, "3"]), false);
+assertEqual(eqArrays([], []), true);
 // => should PASS
 //linted
