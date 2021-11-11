6caf8dd4dc557040d747b866ff9f4a6abd89e0d8
PaolaP <paolaperez8@gmail.com>
2021-11-10 20:41:06 -0500

    dry code

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,9 @@
+const assertObjectsEqual = require("../assertObjectsEqual");
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // => false
