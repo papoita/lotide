07a099aa4ddeabe3c33da57a4987338329e3748a
PaolaP <paolaperez8@gmail.com>
2021-11-10 20:25:38 -0500

    dry code and comments

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,14 @@
-// FUNCTION IMPLEMENTATION
+//DESCRIPTION
+// a function that takes in 2 values and returns true if they are equal and false otherwise
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`); //using template literals
   } else if (actual !== expected) {
     console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
 
+//for exporting we are passing the variable assertEqual and not calling the function assertEqual()
 module.exports = assertEqual;
-//we are passing the variable assertEqual and not calling the function assertEqual()
-//linted
+
