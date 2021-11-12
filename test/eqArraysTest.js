a84e0d805cad5a6158803cc25fe4da22c2070014
PaolaP <paolaperez8@gmail.com>
2021-11-11 19:33:18 -0500

    cleaning up code and linted

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,14 +1,6 @@
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-/*
-
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-*/
 
 //test
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
@@ -17,4 +9,11 @@ assertEqual(eqArrays([1, 2, "3"], [1, 2, "3"]), true);
 assertEqual(eqArrays([1, 2, 3, 4], [1, 2, "3"]), false);
 assertEqual(eqArrays([], []), true);
 // => should PASS
-//linted
+/*
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+*/
